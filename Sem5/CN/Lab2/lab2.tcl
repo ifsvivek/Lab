1315dee4d3df932b744eeee7fd55dbbbef9b04db
vivek <vs63961@gmail.com>
2024-10-03 12:09:05 +0530

    Refactor code to use printf instead of print for better formatting

--- Sem5/CN/Lab2/lab2.tcl
+++ Sem5/CN/Lab2/lab2.tcl
@@ -17,21 +17,25 @@ set n2 [$ns node]
 set n3 [$ns node]
 set n4 [$ns node]
 set n5 [$ns node]
+
 $n0 label "ping0"
 $n1 label "ping1"
 $n2 label "R1"
 $n3 label "R2"
 $n4 label "ping4"
 $n5 label "ping5"
+
 $ns color 1 red
 $ns color 2 blue
 $ns color 3 green
 $ns color 4 orange
+
 $ns duplex-link $n0 $n2 1Mb 10ms DropTail
 $ns duplex-link $n1 $n2 1Mb 10ms DropTail
 $ns duplex-link $n2 $n3 0.4Mb 30ms DropTail
 $ns duplex-link $n3 $n4 1Mb 10ms DropTail
 $ns duplex-link $n3 $n5 1Mb 10ms DropTail
+
 set ping0 [new Agent/Ping]
 $ns attach-agent $n0 $ping0
 set ping1 [new Agent/Ping]
@@ -42,6 +46,7 @@ set ping5 [new Agent/Ping]
 $ns attach-agent $n5 $ping5
 $ns connect $ping0 $ping4
 $ns connect $ping1 $ping5
+
 proc sendPingPacket {} {
     global ns ping0 ping1
     set intervalTime 0.001
@@ -50,6 +55,7 @@ proc sendPingPacket {} {
     $ns at [expr $now + $intervalTime] "$ping1 send"
     $ns at [expr $now + $intervalTime] "sendPingPacket"
 }
+
 Agent/Ping instproc recv {from rtt} {
     global seq
     $self instvar node_
